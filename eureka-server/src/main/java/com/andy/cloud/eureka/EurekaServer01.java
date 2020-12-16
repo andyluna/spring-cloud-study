6cf4a5c10a48d97494fffee861e9aaf0fd1b283e
andyluna <xiangdan311@163.com>
2020-12-16 17:17:35 +0800

    spring-could-study 项目初始化

---
+++ eureka-server/src/main/java/com/andy/cloud/eureka/EurekaServer01.java
@@ -0,0 +1,28 @@
+package com.andy.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @time: 2020/12/16 十二月 15:24
+ * @author: xiangdan/xiangdan@dtxytech.com
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer01 {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context =
+                new SpringApplicationBuilder(EurekaServer01.class)
+                        .web(WebApplicationType.SERVLET)
+                        .run(args);
+    }
+
+
+}
